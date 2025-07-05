c2c17f794a5e07307645f7a8b70f9480cb13380e
leesangdal <asksa1256@gmail.com>
2025-07-05 22:46:01 +0900

    feat: short-link 수정,삭제 연결

--- pages/api/short-links/[id].js
+++ pages/api/short-links/[id].js
@@ -1,14 +1,31 @@
-export default function handler(req, res) {
+import dbConnect from "@/db/dbConnect";
+import ShortLink from "@/db/models/ShortLink";
+
+export default async function handler(req, res) {
   // return res.send(req.query);
   // return res.send(req.body);
   // return res.send(req.cookies);
+  await dbConnect();
+  const { id } = req.query;
+
   switch (req.method) {
     case "GET":
-      res.send("ShortLink 조회");
+      const shortLink = await ShortLink.findById(id);
+      res.send(shortLink);
       break;
+
     case "PATCH":
-      res.send("ShortLink 수정");
+      const updateShortLink = await ShortLink.findByIdAndUpdate(id, req.body, {
+        new: true, // 업데이트 반영 (이 옵션 적용 안하면 업데이트 이전 데이터만 응답)
+      });
+      res.send(updateShortLink);
       break;
+
+    case "DELETE":
+      await ShortLink.findByIdAndDelete(id);
+      res.status(204).send();
+      break;
+
     default:
       res.send();
       break;
